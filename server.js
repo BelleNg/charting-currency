86c00b20e5d528917567d34fa448373b7c5c9b40
Belle Nguyen <NgBelleK@gmail.com>
2020-09-04 16:15:11 -0500

    express server set up

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const path = require('path');
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.static(__dirname + '/public'))
+//will change using path
+
+app.listen(PORT);
+ 
+app.get('/', (req, res) => {
+ res.send({working: true})
+})
