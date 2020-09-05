85899c29588cf4b4e8a30f681a5b6cae6de72327
Belle Nguyen <NgBelleK@gmail.com>
2020-09-05 16:42:15 -0500

    add get request from client side

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Axios from 'axios'
 import { Line } from "react-chartjs-2";
 
 class Graph extends React.Component {
@@ -13,11 +14,36 @@ class Graph extends React.Component {
           borderColor: "rgba(0,0,0,1)",
           borderWidth: 2,
           fill: false,
-          data: [65, 59, 80, 81, 56],
+          data: [128.2597, 127.3648, 127.5915, 120.5738, 120.5333],
         },
       ],
     };
+    this.setData = this.setData.bind(this);
+    this.getData = this.getData.bind(this);
   }
+
+  componentDidMount() {
+      this.getData();
+  }
+  
+  setData(dataObj) {
+      const labelsArr = Object.keys(dataObj);
+      const dataArr = Object.values(dataObj);
+      this.setState({ labels: labelsArr});
+  }
+
+
+  getData() {
+    Axios.get('http://localhost:3000/api/coindesk')
+    .then(function (response) {
+      console.log(response);
+      this.setData(response.data.bpi)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
 
   render() {
     return (
