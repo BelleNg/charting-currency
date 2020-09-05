3a2bce74acab026d1757bae77c9059bdba62e14c
Belle Nguyen <NgBelleK@gmail.com>
2020-09-05 11:21:21 -0500

    created simple working server

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const axios = require("axios");
+const cors = require('cors');
+const path = require("path");
 
 const PORT = 3000;
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
-//will change using path
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT);
- 
-app.get('/', (req, res) => {
- res.send({working: true})
-})
+app.use(express.static(__dirname + "/public"));
+// //will change using path
+
+// app.get("/", (req, res) => {
+//   res.send({ working: true });
+// });
+
+app.get("/api/coindesk", (req, res) => {
+    // will need to change start and end in url to get dynamic dates.
+  axios
+    .get(
+      `https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05`
+    )
+    .then(function (response) {
+    //response.data is object containing info from api
+    //response.data.bpi is the data for graph 
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(PORT, function () {
+    console.log('Server listening on port:',PORT);
+  })
